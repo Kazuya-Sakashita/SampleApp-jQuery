a05e50f7c52ab1ecfbbe868ac1d45f14b98b6760
Kazuya-Sakashita <kz0508@gmail.com>
2023-07-09 07:57:55 +0900

    varの代わりにletを使ってtitleを宣言

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,22 +1,18 @@
 $(function () {
   $(".js-text_field").on("keyup", function () {
-    var title = $.trim($(this).val());
+    let title = $.trim($(this).val());
     $.ajax({
       type: "GET",
       url: "/messages/searches",
       data: "title=" + title,
       dataType: "json",
-    })
-      // 以下を追加
-      .done(function (data) {
-        $(".js-messages li").remove();
-
-        // 以下のコードを追加する
-        $(data).each(function (i, message) {
-          $(".js-messages").append(
-            `<li class="message"><a href="/messages/${message.id}">${message.title}</a></li>`
-          );
-        });
+    }).done(function (data) {
+      $(".js-messages li").remove();
+      $(data).each(function (i, message) {
+        $(".js-messages").append(
+          `<li class="message"><a href="/messages/${message.id}">${message.title}</a></li>`
+        );
       });
+    });
   });
 });
